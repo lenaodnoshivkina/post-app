d02fc88f073f2fb82be24780bb464076dd71fb49
Elena <elena.odnoshivkina@mail.ru>
2020-09-17 00:33:50 +0300

    Search Panel

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,12 +16,14 @@ export default class App extends Component{
                 {label: 'Going to learn React', important: true, like: false, id: 1},
                 {label: 'That is so good', important: false, like: false, id: 2},
                 {label: 'I need a break...', important: false, like: false, id: 3}
-            ]
+            ],
+            term: ''
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant=this.onToggleImportant.bind(this);
         this.onToggleLiked=this.onToggleLiked.bind(this);
+        this.onUpdateSearch=this.onUpdateSearch.bind(this);
 
         this.maxId = 4;
     }
@@ -92,11 +94,27 @@ export default class App extends Component{
         })
     }
 
+    searchPost(items, term){
+        if(term.length === 0){
+            return items
+        }
+
+        return items.filter((item) => {
+            return item.label.indexOf(term) > -1
+        });
+    }
+
+    onUpdateSearch(term){
+        this.setState({term})
+    }
+
     render() {
-        const {data} = this.state;
+        const {data, term} = this.state;
 
         const liked = data.filter(item => item.like).length;
         const allPosts = data.length;
+
+        const visiablePosts = this.searchPost(data, term);
 
         return (
             <div className="app">
@@ -105,11 +123,12 @@ export default class App extends Component{
                     allPosts={allPosts}
                 />
                 <div className="search-panel d-flex">
-                    <SearchPanel/>
+                    <SearchPanel
+                        onUpdateSearch={this.onUpdateSearch}/>
                     <PostStatusFilter/>                
                 </div>
                 <PostList 
-                    posts={this.state.data}
+                    posts={visiablePosts}
                     onDelete={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleLiked={this.onToggleLiked}
